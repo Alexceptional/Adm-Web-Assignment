7ba3baca8becfbacbae604b5abe30faa2909f05f
Alex Scott <alexander.scott@tentel.co.uk>
2017-07-17 15:41:48 +0100

    added view structure, empty html pages, some basic css

--- addresses/views.py
+++ addresses/views.py
@@ -6,3 +6,23 @@ from django.shortcuts import render
 def homepage(request):
 
     return render(request, 'home.html', {})
+
+
+def view_person(request):
+
+    return
+
+
+def update_person(request):
+
+    return
+
+
+def view_org(request):
+
+    return
+
+
+def update_org(request):
+
+    return
