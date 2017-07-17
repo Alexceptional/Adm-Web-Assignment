6d53d821f0bb1e83af34d2248f67a9b82ce2dcee
Alex Scott <alexander.scott@tentel.co.uk>
2017-07-17 11:24:32 +0100

    fixed model choices format, fixed model class decl. order

--- addresses/models.py
+++ addresses/models.py
@@ -3,10 +3,63 @@ from django.db import models
 # Create your models here.
 
 
+class Organisation(models.Model):
+
+    """
+    Organisation class. Field names for address are kept distinct from those of the person to
+    avoid conflicts with HTML element IDs.
+
+    """
+
+    org_name = models.CharField(
+        'Organisation Name', max_length=50
+    )
+
+    org_address_line1 = models.CharField(
+        'Address 1', max_length=100
+    )
+
+    org_address_line2 = models.CharField(
+        'Address 2', max_length=100, blank=True
+    )
+
+    org_address_line3 = models.CharField(
+        'Address 3', max_length=100, blank=True
+    )
+
+    org_address_city = models.CharField(
+        'City', max_length=50
+    )
+
+    org_address_county = models.CharField(
+        'County', max_length=50
+    )
+
+    org_address_postcode = models.CharField(
+        'Postcode', max_length=10
+    )
+
+    contact_telephone = models.CharField(
+        'Telephone Number', max_length=12, blank=True
+    )
+
+    contact_email = models.EmailField(
+        'Email Address', max_length=100, blank=True
+    )
+
+    def __str__(self):
+        return self.org_name
+
+
 class Person(models.Model):
 
     title_choices = [
-        'Ms', 'Miss', 'Mrs', 'Mr', 'Master', 'Mx',
+        ('Ms', 'Ms'),
+        ('Miss', 'Miss'),
+        ('Mrs', 'Mrs'),
+        ('Mr', 'Mr'),
+        ('Master', 'Master'),
+        ('Mx', 'Mx'),
     ]
 
     title = models.CharField(
@@ -61,51 +114,3 @@ class Person(models.Model):
 
     def __str__(self):
         return '{0} {1} {2}'.format(self.firstname, self.middlename, self.surname)
-
-
-class Organisation(models.Model):
-
-    """
-    Organisation class. Field names for address are kept distinct from those of the person to
-    avoid conflicts with HTML element IDs.
-
-    """
-
-    org_name = models.CharField(
-        'Organisation Name', max_length=50
-    )
-
-    org_address_line1 = models.CharField(
-        'Address 1', max_length=100
-    )
-
-    org_address_line2 = models.CharField(
-        'Address 2', max_length=100, blank=True
-    )
-
-    org_address_line3 = models.CharField(
-        'Address 3', max_length=100, blank=True
-    )
-
-    org_address_city = models.CharField(
-        'City', max_length=50
-    )
-
-    org_address_county = models.CharField(
-        'County', max_length=50
-    )
-
-    org_address_postcode = models.CharField(
-        'Postcode', max_length=10
-    )
-
-    contact_telephone = models.CharField(
-        'Telephone Number', max_length=12, blank=True
-    )
-
-    contact_email = models.EmailField(
-        'Email Address', max_length=100, blank=True
-    )
-
-    def __str__(self):
-        return self.org_name
