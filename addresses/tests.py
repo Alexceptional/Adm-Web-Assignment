f57bb8f3ee6e33dbecca85ce9041f124050f9c02
Alex Scott <alexander.scott@tentel.co.uk>
2017-07-19 15:27:14 +0100

    added basic unit test for request/resp

--- addresses/tests.py
+++ addresses/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class AddrViewTests(TestCase):
+
+    def test_homepage_resp(self):
+
+        resp = self.client.get('/')
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('people' in resp.context)
+        self.assertTrue('orgs' in resp.context)
